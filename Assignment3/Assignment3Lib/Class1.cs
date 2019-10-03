7cfb6afc72a2b757728a05d6c398340db8373534
joshbremser <joshua.bremser@maine.edu>
2019-10-03 15:31:57 -0400

    Added classes to deserialize JSON into

--- Assignment3/Assignment3Lib/Class1.cs
+++ Assignment3/Assignment3Lib/Class1.cs
@@ -5,4 +5,19 @@ namespace Assignment3Lib
     public class Class1
     {
     }
+
+    public class GoogleResponse
+    {
+        public location[] locations { get; set; }
+
+    }
+
+    public class location
+    {
+        public int timestampMs { get; set; }
+        public int longitudeE7 {get; set;}
+        public int latitudeE7 {get; set;}
+        public int accuracy {get; set;}
+
+    }
 }
